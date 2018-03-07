b4eacd718cfbc695add1602973eff50c96a7d66d
Brynja Gudmundsdottir <brynjagudmundsdottir@Brynjas-MacBook-Pro.local>
2018-03-07 09:29:10 +0000

    third successful test - trying out .toUpperCase

--- textUtilities.js
+++ textUtilities.js
@@ -1,9 +1,10 @@
 var expect = require("chai").expect;
 
-expect(true).to.be.true;
-
 function titleCase(title) {
-    return title;
+    return title.toUpperCase();
 }
 
 expect(titleCase("the great mouse detective")).to.be.a("string");
+expect(titleCase("a")).to.equal("A");
+
+expect(titleCase("the great mouse detective")).to.equal("The Great Mouse Detective");
