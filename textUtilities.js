f288fd2646582b857fca0222a6b072b8c42b0acd
Brynja Gudmundsdottir <brynjagudmundsdottir@Brynjas-MacBook-Pro.local>
2018-03-07 09:32:29 +0000

    third successful test - title.substring

--- textUtilities.js
+++ textUtilities.js
@@ -1,10 +1,11 @@
 var expect = require("chai").expect;
 
 function titleCase(title) {
-    return title.toUpperCase();
+    return title.toUpperCase() + title.substring(1);
 }
 
 expect(titleCase("the great mouse detective")).to.be.a("string");
 expect(titleCase("a")).to.equal("A");
+expect(titleCase("vertigo")).to.equal("Vertigo");
 
 expect(titleCase("the great mouse detective")).to.equal("The Great Mouse Detective");
